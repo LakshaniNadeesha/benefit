453f3d118837c71ed16d42967d9fe1c63d47f51b
Sathya <sathyaudayangi2@gmail.com>
2021-12-21 03:47:34 +0530

    Mark Multiple Employees Attendance And Show Attendance History

--- source/controller/Markattendance.php
+++ source/controller/Markattendance.php
@@ -16,9 +16,17 @@ class Markattendance extends Controller
             $id = auth::user();
             $user = new EmployeelistModel();
             $designations = new DesignationModel();
+            $attendance = new AttendanceModel();
+
             $all_emp = $user->where('supervisor_ID', $id);
             //print_r($all_emp);
 
+            $today = date("Y-m-d");
+            //$today = "2021-12-21";
+
+            $j=0; $k=0;
+            $marked = array();
+            $not_marked = array();
             for($i=0;$i<sizeof($all_emp);$i++){
                 if($all_emp[$i]->designation_code == 1){
                     $all_emp[$i]->designation_code = 'CEO';
@@ -35,7 +43,6 @@ class Markattendance extends Controller
                 elseif ($all_emp[$i]->designation_code == 5){
                     $all_emp[$i]->designation_code = 'Employer';
                 }
-                //print_r($all_emp[$i]->designation_code);
 
                 if($all_emp[$i]->department_ID == 1){
                     $all_emp[$i]->department_ID = 'Operational Department';
@@ -49,25 +56,62 @@ class Markattendance extends Controller
                 elseif ($all_emp[$i]->department_ID == 4){
                     $all_emp[$i]->department_ID = 'Account Department';
                 }
-                //print_r($all_emp[$i]->department_ID);
+
+                //Filter today's not marked employees
+                $array1 = $attendance->where_condition('employee_ID','date',$all_emp[$i]->employee_ID,$today);
+
+                if(boolval($array1)){
+                    //$marked[$j] = $all_emp[$i];
+                    //$j++;
+                    continue;
+                }
+                else {
+                    $not_marked[$k] = $all_emp[$i];
+                    $k++;
+                }
             }
 
-            //Marking Attendance
-            $attendance = new AttendanceModel();
+
+            //Getting form data and send it to database
 
             if(count($_POST) > 0){
                 if(isset($_POST['submit'])){
-                    $arr['employee_ID'] = $_POST['employee_ID'];
-                    $arr['date'] = $_POST['date'];
-                    $arr['arrival_time'] = $_POST['arrival'];
-                    $arr['departure_time'] = $_POST['departure'];
-                    $arr['ot_hours'] = $_POST['ot-hours'];
-                    $attendance->insert($arr);
+                    $checkbox = $_POST['person'];
+                    $date = $_POST['date'];
+                    $arrival_time = $_POST['arrival'];
+                    $departure_time = $_POST['departure'];
+                    $ot_hour = $_POST['ot-hours'];
+                    $t1 = strtotime($arrival_time);
+                    $t2 = strtotime($departure_time);
+                    $hours = ($t2 - $t1)/3600;
+
+                    foreach ($checkbox as $chk) {
+                        $arr['employee_ID'] = $chk;
+                        $arr['date'] = $date;
+                        $arr['arrival_time'] = $arrival_time;
+                        $arr['departure_time'] = $departure_time;
+                        $arr['ot_hours'] = $ot_hour;
+
+                        if($hours >= '8:30'){
+                            $arr['status'] = 'Yes';
+                        }
+                        elseif($hours == '4:00'){
+                            $arr['status'] = 'Half-Day';
+                        }
+                        else {
+                            $arr['status'] = 'No';
+                        }
+
+                        $attendance->insert($arr);
+                    }
                     $this->redirect('markattendance');
                 }
             }
 
-            $this->view('markattendance', ['details'=> $all_emp]);
+            //Show attendance history
+            $history = $attendance->findAll();
+
+            $this->view('markattendance', ['not_marked'=>$not_marked, 'history'=>$history]);
         } else {
             $this->view('404');
         }
--- source/views/markattendance.view.php
+++ source/views/markattendance.view.php
@@ -11,6 +11,21 @@
 
     <title></title>
 </head>
+<script>
+    function date_validation() {
+        var t1 = document.forms["attendance-form"]["arrival"].value;
+        var t2 = document.forms['attendance-form']["departure"].value;
+        console.log("t1");
+        document.getElementById("arrival").innerHTML = t1;
+
+        var t1date = new Date("01/01/2021" + t1);
+        var t2date = new Date("01/01/2021" + t2);
+
+        if (t1date < t2date) {
+            document.getElementById("arrival").innerHTML = "<div style='font-family: Arial,serif; font-size: smaller; color: red'><i class='fas fa-exclamation' style='color: red;'></i></div>";
+        }
+    }
+</script>
 
 <body>
 <div>
@@ -43,58 +58,125 @@
             </div>
         </div>
         <div class="emp_list">
-            <p class="title">Employee List</p>
-            <form name="" action="" method="post">
-                <?php
-                if (boolval($details)) {
-                    for ($i = 0; $i < sizeof($details); $i++) {
-                        ?>
-                        <div class="box">
-                            <img src="<?php print_r($details[$i]->profile_image) ?>" alt="on-leave-people"
-                                 class="on-leave-people">
-                            <p class="content"><?php print_r($details[$i]->first_name);
-                                echo " ";
-                                print_r($details[$i]->last_name) ?></p>
-                            <p class="content"><?php print_r($details[$i]->department_ID) ?></p>
-                            <p class="content"><?php print_r($details[$i]->designation_code) ?></p>
-                            <?php
-                            $select = 'select';
-                            $select .= $i;
+            <h class="title">Employee List</h>
+            <div>
+                <p>To Be Add</p>
+                <hr>
+                <form name="attendance-form" action="" method="post" onsubmit="return date_validation();"
+                      enctype="multipart/form-data">
+                    <?php
+                    if (boolval($not_marked)) {
+                        for ($i = 0; $i < sizeof($not_marked); $i++) {
                             ?>
-                            <input type="checkbox" id="<?php echo $select ?>" name="">
-                        </div>
-                        <script>
-                            const hideBox = document.querySelector('<?php echo "#" . $select;?>');
-                            hideBox.addEventListener('change', function (e) {
-                                if (hideBox.checked) {
-                                    list.style.display = "initial";
-                                } else {
-                                    list.style.display = "none";
-                                }
-                            });
-                        </script>
+                            <div class="box">
+                                <img src="<?php print_r($not_marked[$i]->profile_image) ?>" alt="on-leave-people"
+                                     class="on-leave-people">
+                                <p class="content"><?php print_r($not_marked[$i]->first_name);
+                                    echo " ";
+                                    print_r($not_marked[$i]->last_name) ?></p>
+                                <p class="content"><?php print_r($not_marked[$i]->department_ID) ?></p>
+                                <p class="content"><?php print_r($not_marked[$i]->designation_code) ?></p>
+                                <?php
+                                $select = 'select';
+                                $select .= $i;
+                                ?>
+                                <input type="checkbox" id="<?php echo $select ?>" name="person[]"
+                                       value="<?php print_r($not_marked[$i]->employee_ID); ?>">
+                            </div>
+                            <script>
+                                const hideBox = document.querySelector('<?php echo "#" . $select;?>');
+                                hideBox.addEventListener('change', function (e) {
+                                    if (hideBox.checked) {
+                                        list.style.display = "initial";
+                                    } else {
+                                        list.style.display = "none";
+                                    }
+                                });
+                            </script>
+                            <?php
+                        }
+                    } ?>
+            </div>
+            <div>
+                <p>Attendance History</p>
+                <hr>
+                <?php if (boolval($history)){ ?>
+                <table>
+                    <thead>
+                        <th>Name</th>
+                        <th>Arrival</th>
+                        <th>Departure</th>
+                        <th>Date</th>
+                        <th>OT Hours</th>
+                        <th>Status</th>
+                        <th>Option</th>
+                    </thead>
+                    <tbody>
+                    <?php
+                    for($i=0;$i<sizeof($history);$i++){ ?>
+                    <tr>
+                        <td><?php print_r($history[$i]->employee_ID); ?></td>
+                        <td><?php print_r($history[$i]->arrival_time); ?></td>
+                        <td><?php print_r($history[$i]->departure_time); ?></td>
+                        <td><?php print_r($history[$i]->date); ?></td>
+                        <td><?php print_r($history[$i]->ot_hours); ?></td>
+                        <td><?php print_r($history[$i]->status); ?></td>
                         <?php
-                    }
-                } ?>
+                        $btnChange = 'btnChange';
+                        $btnChange .= $i;
+                        $btnDelete = 'btnDelete';
+                        $btnDelete .= $i;
+                        //echo $btnChange;
+                        ?>
+                        <td id="options"><div id="<?php echo $btnChange ?>" onclick="reply_click(this.id)"><i class="fas fa-pencil-alt"></i></div>
+                            <script type="text/javascript">
+                                document.querySelector('<?php echo "#".$btnChange;?>').addEventListener('click', () => {
+                                    Change.open({
+                                        title: 'Changing..',
+                                        code: '<?php print_r($history[$i]->employee_ID) ?>',
+                                        type: '<?php print_r($history[$i]->arrival_time) ?>',
+                                        amount: '<?php print_r($history[$i]->departure_time) ?>',
+                                        months: '<?php print_r($history[$i]->date) ?>',
+                                        ot : '<?php print_r($history[$i]->ot_hours) ?>',
+                                        years: '<?php print_r($history[$i]->status) ?>',
+                                        href: '<?php echo"change/"; print_r($history[$i]->employee_ID); ?>',
+                                        onchange: () => {
+                                            window.location.href = "<?php print_r($history[$i]->employee_ID); ?>"
+                                        },
+                                    })
+                                });
+                            </script>
+                        </td>
+                    </tr>
+                    <?php
+                    } ?>
+                    </tbody>
+                </table>
+                <?php
+                }
+                else {
+                    echo "No history yet";
+                }?>
+            </div>
         </div>
         <div class="attendance_form">
             <div class="form-title">Fill This</div>
             <div class="form-content">
                 <div class="date">
                     <label for="date">Date : </label>
-                    <input type="date" name="date" id="name" value="<?php echo date('Y-m-d'); ?>" readonly>
+                    <input type="date" name="date" id="name" value="<?php echo date('Y-m-d'); ?>" required >
                 </div>
                 <div class="selected-names">
                     <?php
-                    if (boolval($details)) {
-                        for ($i = 0; $i < sizeof($details); $i++) {
+                    if (boolval($not_marked)) {
+                        for ($i = 0; $i < sizeof($not_marked); $i++) {
                             $list = 'list';
                             $list .= $i;
                             ?>
                             <div class="box" id="<?php echo $list ?>">
-                                <img src="<?php print_r($details[$i]->profile_image) ?>" alt="on-leave-people"
+                                <img src="<?php print_r($not_marked[$i]->profile_image) ?>" alt="on-leave-people"
                                      class="on-leave-people">
-                                <p class="content"><?php print_r($details[$i]->first_name); ?></p>
+                                <p class="content"><?php print_r($not_marked[$i]->first_name); ?></p>
                             </div>
                             <script>
                                 const list = document.querySelector('<?php echo "#" . $list;?>');
@@ -104,34 +186,151 @@
                     } ?>
                 </div>
                 <div class="row">
-                    <label for="name">Name</label>
-                    <input type="text" name="name" id="name" required>
-                </div>
-                <div class="row">
-                    <label for="employee_ID">ID</label>
-                    <input type="text" name="employee_ID" id="employee_ID" required>
-                </div>
-                <div class="row">
                     <label for="arrival">Arrival</label>
-                    <input type="time" name="arrival" id="arrival" required>
+                    <input type="time" name="arrival" id="arrival" value="08:00:00" required>
                 </div>
                 <div class="row">
                     <label for="departure">Departure</label>
-                    <input type="time" name="departure" id="departure" required>
+                    <input type="time" name="departure" id="departure" value="16:30:00" required>
                 </div>
                 <div class="row">
                     <label for="ot-hours">OT Hours</label>
-                    <input type="number" name="ot-hours" id="ot-hours">
+                    <input type="number" name="ot-hours" id="ot-hours" value="0" min="0">
                 </div>
                 <div class="buttons">
                     <button type="reset" class="cancel">Cancel</button>
-                    <input type="submit" class="mark" value="submit" name="submit">
+                    <input type="submit" class="mark" value="Mark" name="submit">
                 </div>
             </div>
             </form>
         </div>
     </div>
+</div>
+<script>
+    const  Change = {
+        open(options){
+            options = Object.assign({},{
+                title: '',
+                message: '',
+                type: '',
+                code: '',
+                amount: '',
+                months: '',
+                ot: '',
+                years: '',
+                href: '',
+                cancelText: 'Cancel',
+                onchange: function () {},
+                oncancel: function () {}
+            }, options);
+
+
+            const change_html = `<div class="confirm">
+    <div class="confirm__window">
+        <div class="confirm__titlebar">
+            <span class="confirm__title">${options.title}</span>
+            <button class="confirm__close">&times;</button>
+        </div>
+        <div class="confirm__content">${options.message}
+            <div class="benefit_head" id="myForm">
+
+                <div class="benefit_form">
+
+                    <form action="" method="post" autocomplete="off">
+                        <div class="row">
+                            <div class="column_1">
+                                <label for="emp_name">Name</label>
+                            </div>
+                            <div class="column_2">
+                                <input type="text" id="emp_name" name="emp_name" value="${options.code}" readonly>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="column_1">
+                                <label for="status">Date</label>
+                            </div>
+                            <div class="column_2">
+                                <input type="date" id="date" name="date" value="${options.months}" readonly>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="column_1">
+                                <label for="arrival">Arrival</label>
+                            </div>
+                            <div class="column_2">
+                                <input type="time" id="arrival" name="arrival" value="${options.type}" required >
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="column_1">
+                                <label for="departure">Departure</label>
+                            </div>
+                            <div class="column_2">
+                                <input type="time" id="departure" name="departure" value="${options.amount}" required>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="column_1">
+                                <label for="ot_hours">OT Hours</label>
+                            </div>
+                            <div class="column_2">
+                                <input type="number" id="ot_hours" name="ot_hours" value="${options.ot}" required>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="column_1">
+                                <label for="status">Status</label>
+                            </div>
+                            <div class="column_2">
+                                <input type="text" id="status" name="status" value="${options.years}" required>
+                            </div>
+                        </div>
+
+                        <div class="confirm__buttons">
+                            <button class="confirm__button confirm__button--cancel" type="reset">${options.cancelText}</button>
+                            <button class="confirm__button confirm__button--ok confirm__button--fill" type="submit" value="Change" name="submit">Change</button>
+                        </div>
+                    </form>
+
+        </div>
+        </div>
+
+    </div>
+</div>`;
+
+            const template_3 = document.createElement('template');
+            template_3.innerHTML = change_html;
+
+            const confirmEl = template_3.content.querySelector('.confirm');
+            const btnClose = template_3.content.querySelector('.confirm__close');
+            const btnchange = template_3.content.querySelector('.confirm__button--ok');
+            const btnCancel = template_3.content.querySelector('.confirm__button--cancel');
+
+            confirmEl.addEventListener('click', e => {
+                if(e.target === confirmEl){
+                    options.oncancel();
+                    this._close(confirmEl);
+                }
+            });
+
+            [btnCancel, btnClose].forEach(el => {
+                el.addEventListener('click', () => {
+                    options.oncancel();
+                    this._close(confirmEl);
+                });
+            });
+
+            document.body.appendChild(template_3.content);
+        },
+
+        _close (confirmEl){
+            confirmEl.classList.add('confirm--close');
+            confirmEl.addEventListener('animationend', () => {
+                document.body.removeChild(confirmEl);
+            });
+        }
+    }
+</script>
 </body>
-
-
 </html>
