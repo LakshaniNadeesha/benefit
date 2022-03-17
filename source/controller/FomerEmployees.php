f6db91529df2d560e835600c218e8d2f810a1ff9
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-03-18 00:05:41 +0530

    Update FomerEmployees.php

--- source/controller/FomerEmployees.php
+++ source/controller/FomerEmployees.php
@@ -3,5 +3,18 @@
 /**
  * former employees controller
  */
+function index(){
+        if(!Auth::logged_in())
+        {
+            $this->redirect('login');
+        }
+        $row =array();
+
+        $user=new Employeedetails();
+        $row=$user->where('banned_employees',1);
+        //print_r($row);
+        $this->view('fomeremployees',['row'=>$row]);
+
+    }
 
 }
