6e8acd2a814bcc7a1be104e5955961438759d4d9
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-03-19 20:01:14 +0530

    Update LeaveapproveController.php

--- source/controller/LeaveapproveController.php
+++ source/controller/LeaveapproveController.php
@@ -7,56 +7,68 @@ class LeaveapproveController extends Controller
     function index()
     {
 
-        if (!Auth::logged_in()) {
+             if (!Auth::logged_in()) { 
             $this->redirect('login');
         }
         if (Auth::access('Supervisor') || Auth::access('HR Manager')) {
-            $user=new Employeedetails();
+            $user=new EmployeelistModel();
             $user_x = new RequestleaveModel();
             $id=Auth::user();
             $row=$user->where('supervisor_ID',$id);
-            $emp=array();
+             $emp=array();
+             $emps=array();
             $k=0;
                 if(boolval($row)){
                 for($i = 0;$i<sizeof($row);$i++){
-                    // $employee_details = $user->where_condition('employee_ID','banned_employees',$row[$i]->employee_ID,0);
-
-                    $employee_details = $user->where('employee_ID', $row[$i]->employee_ID );
+                    $employee_details = $user->where_condition('employee_ID','banned_employees',$row[$i]->employee_ID,0);
                     if(boolval($employee_details)){
                     $leave_details = $user_x->where_condition('employee_ID', 'leave_status', $row[$i]->employee_ID, 'Pending');
                     if(boolval($leave_details)){
-                  
+                            //print_r($leave_details);
+                            $emp[$k]['employee_ID'] = $employee_details[0]->employee_ID;
+                            $emp[$k]['first_name'] = $employee_details[0]->first_name;
+                            $emp[$k]['last_name'] = $employee_details[0]->last_name;
+                            $emp[$k]['profile_image'] = $employee_details[0]->profile_image;
+                            $empss[$k]=sizeof($leave_details);
+                            //print_r($empss);
                         for($j = 0;$j<sizeof($leave_details);$j++){
-                            $emp[$j]['employee_ID'] = $employee_details[0]->employee_ID;
-                            $emp[$j]['first_name'] = $employee_details[0]->first_name;
-                            $emp[$j]['last_name'] = $employee_details[0]->last_name;
-                            $emp[$j]['profile_image'] = $employee_details[0]->profile_image;
-                            $emp[$j]['details'] = $leave_details[$j];
+
+                            // $emp[$k]['employee_ID'] = $employee_details[0]->employee_ID;
+                            // $emp[$k]['first_name'] = $employee_details[0]->first_name;
+                            // $emp[$k]['last_name'] = $employee_details[0]->last_name;
+                            // $emp[$k]['profile_image'] = $employee_details[0]->profile_image;
+                            $emp[$k]['details'][$j] = $leave_details[$j];
+                              //print_r($emp)[$k]['details'][$j];
                         }
-               
-                        $i = $i+sizeof($leave_details)-1;
+                        
+                        // $i = $i+sizeof($leave_details)-1;
                         $k++;
                     }
                 }
                 }
-                $l=0;
+                               $l=0;
                   for($i= 0;$i<sizeof($row);$i++){
-                    // $employee_detailss = $user->where_condition('employee_ID','banned_employees',$row[$i]->employee_ID,0);
-                    $employee_detailss = $user->where('employee_ID', $row[$i]->employee_ID );
-
+                    $employee_detailss = $user->where_condition('employee_ID','banned_employees',$row[$i]->employee_ID,0);
                     if(boolval($employee_detailss)){
+                        //print_r($employee_detailss);
                     $leave_detailss = $user_x->where_condition('employee_ID', 'leave_status', $row[$i]->employee_ID, 'Success');
+                    //print_r($leave_detailss);
                     if(boolval($leave_detailss)){
                   
+                            $emps[$l]['employee_ID'] = $employee_detailss[0]->employee_ID;
+                            $emps[$l]['first_name'] = $employee_detailss[0]->first_name;
+                            $emps[$l]['last_name'] = $employee_detailss[0]->last_name;
+                            $emps[$l]['profile_image'] = $employee_detailss[0]->profile_image;
+                            $empsss[$l]=sizeof($leave_detailss);
                         for($j = 0;$j<sizeof($leave_detailss);$j++){
-                            $emps[$l]['employee_ID'] = $employee_details[0]->employee_ID;
-                            $emps[$l]['first_name'] = $employee_details[0]->first_name;
-                            $emps[$l]['last_name'] = $employee_details[0]->last_name;
-                            $emps[$l]['profile_image'] = $employee_details[0]->profile_image;
-                            $emps[$l]['details'] = $leave_detailss[$l];
+                            // $emps[$l]['employee_ID'] = $employee_detailss[0]->employee_ID;
+                            // $emps[$l]['first_name'] = $employee_detailss[0]->first_name;
+                            // $emps[$l]['last_name'] = $employee_detailss[0]->last_name;
+                            // $emps[$l]['profile_image'] = $employee_detailss[0]->profile_image;
+                            $emps[$l]['details'][$j] = $leave_detailss[$j];
                         }
                
-                        $i = $i+sizeof($leave_detailss)-1;
+                        // $i = $i+sizeof($leave_detailss)-1;
                         $l++;
                     }
                 }
@@ -65,7 +77,7 @@ class LeaveapproveController extends Controller
             }
 
             // echo "jfnbnenbnoeno";
-        $this->view('leaveapprove',['emp'=>$emp,'emps'=>$emps]);
+              $this->view('leaveapprove',['emp'=>$emp,'emps'=>$emps,'empss'=>$empss,'empsss'=>$empsss]);
         // if(count($_POST)> 0){
             // echo "jfnbnenbnoeno";
             // if(isset($_POST['button'])){
