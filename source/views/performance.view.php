9f38a5e8d970b96d94a172d7f01e563395cdc49b
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-03-21 20:09:00 +0530

    Update performance.view.php

--- source/views/performance.view.php
+++ source/views/performance.view.php
@@ -8,7 +8,7 @@
     <!-- <link rel="stylesheet" href="<?= CSS_PATH ?>header2.css"> -->
     <link rel="stylesheet" href="<?= CSS_PATH ?>performance.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <title>Performance</title>
+    <title></title>
 </head>
 <body>
 <div>
@@ -31,24 +31,28 @@
             <div class="heading">
                 <h2>PERFORMANCES</h2>
             </div>
+            <div class="heading">
+                <h4>Last Three Months Performances</h2>
+            </div>
             <div>
                 <div class="conta">
                     <div class="row">
-                        <div class="col-6 chart">
-                            <canvas id="myChart2"></canvas>
+                        <div class="col-6 chart" >
+                            <canvas id="myChart2" ></canvas>
                         </div>
                     </div>
                 </div>
                 <?php
+                //print_r($row);
                 if (boolval($row) > 0) {
-                    $row = [$row[0]->communication, $row[0]->quality_of_work, $row[0]->organization, $row[0]->team_skills, $row[0]->multitasking_ability];
+                    $rows = [$row[0]->communication, $row[0]->quality_of_work, $row[0]->organization, $row[0]->team_skills, $row[0]->multitasking_ability];
 
 
                     ?>
 
                     <script>
                         var labels2 = ['Communication', 'Quality of work', 'Organization', 'Team skills', 'Multitasking ability'];
-                        var data2 = <?=json_encode($row)?>;
+                        var data2 = <?=json_encode($rows)?>;
                         var colors2 = ['#a88d32', '#73a832', '#32a89e', '#8f72e8', '#a274a6'];
 
                         var ctx = document.getElementById("myChart2").getContext('2d');
@@ -60,6 +64,58 @@
                                 datasets: [{
                                     data: data2,
                                     backgroundColor: colors2
+
+                                }]
+                            },
+                            options: {
+                                title: {
+
+                                    display: true
+                                },
+                                legend: {
+                                    display: false
+                                }
+                            }
+                        });
+
+                    </script>
+            </div>
+        </div>
+        <div class="performance_details">
+            <div class="heading">
+                <h4>Overall Performances</h2>
+            </div>
+            <div>
+                <div class="conta">
+                    <div class="row">
+                        <div class="col-6 chart" >
+                            <canvas id="myChart3" style="height:40vh; width:60vw"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                //print_r($row->communication_overall);
+                //if (boolval($row) > 0) {
+                    $rowss = [$row[0]->communication_overall, $row[0]->quality_of_work_overall, $row[0]->organization_overall, $row[0]->team_skills_overall, $row[0]->multitasking_ability_overall];
+
+
+                    ?>
+
+                    <script>
+                        var labels3 = ['Communication_overall', 'Quality of work_overall', 'Organization_overall', 'Team skills_overall', 'Multitasking ability_overall'];
+                        var data3 = <?=json_encode($rowss)?>;
+                        var colors3 = ['#a88d32', '#73a832', '#32a89e', '#8f72e8', '#a274a6'];
+
+                        var ctx = document.getElementById("myChart3").getContext('2d');
+
+                        var myChart3 = new Chart(ctx, {
+                            type: 'bar',
+                            data: {
+                                labels: labels3,
+                                datasets: [{
+                                    data: data3,
+                                    backgroundColor: colors3
+
                                 }]
                             },
                             options: {
@@ -76,9 +132,11 @@
                     </script>
                 <?php }
                 else{
-                ?>
+                    ?>
                     <h3>You don't have records yet</h3>
-                <?php } ?>
+                <?php 
+                    }
+             ?>
             </div>
         </div>
     </div>
@@ -88,3 +146,4 @@
 <!--</div>-->
 </body>
 </html>
+
