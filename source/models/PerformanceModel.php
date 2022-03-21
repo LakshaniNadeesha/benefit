ad2df0e20c1ceee4d34dfb28e27b6ac26dcc26fe
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-03-21 20:05:07 +0530

    Update PerformanceModel.php

--- source/models/PerformanceModel.php
+++ source/models/PerformanceModel.php
@@ -12,31 +12,31 @@ class PerformanceModel extends Model
 	{
 		$this->errors = array();
 		//check for commu
-		$communications=[1,25,50,75,100];
+		$communications=[0,25,50,75,100];
 		if(empty($DATA['communication'])|| !in_array($DATA['communication'], $communications))
 		{
 			$this->errors['communication']="Communication is not valid";
 		}
 		//check for quality_of_work
-		$quality_of_works=[1,25,50,75,100];
+		$quality_of_works=[0,25,50,75,100];
 		if(empty($DATA['quality_of_work'])|| !in_array($DATA['quality_of_work'], $quality_of_works))
 		{
 			$this->errors['quality_of_work']="Quality of work is not valid";
 		}
 		//check for organization
-		$organizations=[1,25,50,75,100];
+		$organizations=[0,25,50,75,100];
 		if(empty($DATA['organization'])|| !in_array($DATA['organization'], $organizations))
 		{
 			$this->errors['organization']="organization is not valid";
 		}
 		//check for team_skills
-		$team_skillss=[1,25,50,75,100];
+		$team_skillss=[0,25,50,75,100];
 		if(empty($DATA['team_skills'])|| !in_array($DATA['team_skills'], $team_skillss))
 		{
 			$this->errors['team_skills']="Team skills is not valid";
 		}
 		//check for multi
-		$multitasking_abilitys=[1,25,50,75,100];
+		$multitasking_abilitys=[0,25,50,75,100];
 		if(empty($DATA['multitasking_ability'])|| !in_array($DATA['multitasking_ability'], $multitasking_abilitys))
 		{
 			$this->errors['multitasking_ability']="Multitasking ability is not valid";
