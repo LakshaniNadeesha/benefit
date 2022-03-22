f4e7149e7fc041ef3e75e998256c89358a8908f5
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-03-22 12:53:01 +0530

    Update Outcomes.php

--- source/controller/Outcomes.php
+++ source/controller/Outcomes.php
@@ -36,4 +36,28 @@ class Outcomes extends Controller
 
 		$this->view('outcomes.addpost');
 	}
+	
+	function updatepost($id){
+		//print_r($id);
+		$user=new OutcomesModel();
+		$row=$user->where('post_id',$id); 
+		// if(boolval($row)){
+		// 	$errors="Wrong Post Edite Try agin later";
+		// }
+		//$row=$row[0];
+		 if(isset($_POST)){
+			
+			if(count($_POST)>0){
+
+				$arr['title']=$_POST['title'];
+				$arr['description']=$_POST['outcomes'];
+				$arr['date']=$_POST['date'];
+				$user=new OutcomesModel();
+				$row=$user->update_status($id,'post_id',$arr);
+				$this->redirect('outcomes');
+			}
+		}
+
+		$this->view('outcomesupdate',['row'=>$row]);
+	}
 }
