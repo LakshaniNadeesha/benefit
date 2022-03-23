3d6ebf715f123478b36022f96c85ffc1e357bb5b
LakshaniNadeesha <2019cs105@stu.ucsc.lk>
2022-03-23 13:01:01 +0530

    Change Update Outcomes View and CSS

--- source/views/outcomesupdate.view.php
+++ source/views/outcomesupdate.view.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href=" <?= CSS_PATH ?>outcome.css">
-    <link rel="stylesheet" href="public\css\outcome.css">
+    <!-- <link rel="stylesheet" href=" <?//= CSS_PATH ?>outcome.css"> -->
+    <link rel="stylesheet" href=" <?= CSS_PATH ?>outcomesupdate.css">
 
     <title></title>
 </head>
@@ -19,7 +19,16 @@
 
 <div class="profile_container1">
 
-    <div class="row">
+    <div class="row1">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 50 1440 185"><path fill="#0f9eb8" fill-opacity="1" d="M0,64L26.7,58.7C53.3,
+        53,107,43,160,80C213.3,117,267,203,320,218.7C373.3,235,427,181,480,181.3C533.3,181,587,235,640,229.3C693.3,224,747,160,800,
+        128C853.3,96,907,96,960,112C1013.3,128,1067,160,1120,176C1173.3,192,1227,192,1280,170.7C1333.3,149,1387,107,1413,85.3L1440,
+        64L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,
+        0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"></path></svg>
+
+        <div class="back_btn">
+        <a href="<?= PATH ?>Outcomes"><i class="large material-icons">arrow_back</i></a>
+                </div>
         <div class="title">Meeting Outcomes</div>
         <div class="leftcolumn">
   
@@ -42,7 +51,7 @@
                     </div>
                     <div class="buttons">
                         <input type="reset" value="Cancel">
-                        <input type="submit" name="submit" value="Submit">
+                        <input type="submit" name="submit" value="Update">
                     </div>
                 </div>
             </form>
