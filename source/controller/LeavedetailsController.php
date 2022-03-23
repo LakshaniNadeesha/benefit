a97feca16ee2ada78e60f835706e39d4fb9827b5
Dilukshan Bimsara <dilukshanbimsara@gmail.com>
2022-03-23 15:21:43 +0530

    Add Leave History

--- source/controller/LeavedetailsController.php
+++ source/controller/LeavedetailsController.php
@@ -4,7 +4,53 @@ class LeavedetailsController extends Controller {
     
     function index(){
         $user = new LeavedetailsModel();
-        $this->view('leavedetails');
+        $user_x = new RequestleaveModel();
+
+        $id = Auth::user();
+
+        $leave_list = $user_x->where('employee_ID',$id);
+
+   
+        $arr1 = array();
+        $arr2 = array();
+        $arr3 = array();
+        $sick = 0;
+        $casual = 0;
+        $annual = 0;
+
+        if(boolval($leave_list)){
+            for($i=0; $i<sizeof($leave_list); $i++){
+                $today = date("Y-m-d");
+                $new_date = $leave_list[$i]->date;
+    
+                if($today <= $new_date){
+                    array_push($arr1,$leave_list[$i]);
+                }
+
+                if($leave_list[$i]->leave_status === "reject" || $leave_list[$i]->leave_status === "approve"){
+                    array_push($arr2,$leave_list[$i]);
+                    if($leave_list[$i]->leave_status === "approve" && $leave_list[$i]->leave_type == "sick"){
+                        $sick = $sick+1;
+                    }elseif($leave_list[$i]->leave_status === "approve" && $leave_list[$i]->leave_type == "casual"){
+                        $casual = $casual +1;
+                    }elseif($leave_list[$i]->leave_status === "approve" && $leave_list[$i]->leave_type == "annual"){
+                        $annual = $annual + 1;
+                    }
+
+                    $arr3['casual'] = $casual;
+                    $arr3['annual']= $annual;
+                    $arr3['sick'] = $sick;
+                }
+            }
+
+
+        }
+        
+
+        
+
+
+        $this->view('leavedetails',['arr1'=>$arr1 , 'arr2'=>$arr2, 'arr3'=>$arr3]);
         // private\models\LeavedetailsController.php
         // C:\xampp\htdocs\benefit\private\views\leavedetails.view.php
         // C:\xampp\htdocs\benefit\private\models\LeavedetailsModel.php
--- source/views/leavedetails.view.php
+++ source/views/leavedetails.view.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,150 +13,164 @@
     <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
     <title>Leaves</title>
 </head>
+
 <body>
-<div>
-    <?php
-    $this->view('includes/header1')
-    ?>
-
-</div>
-
-<div class="profile_container">
-    <div class="profile">
+    <div>
         <?php
-        $this->view('includes/profile1');
+        $this->view('includes/header1')
         ?>
+
     </div>
 
-    <div class="content">
-        <div>
+    <div class="profile_container">
+        <div class="profile">
             <?php
-            $this->view('includes/header2')
+            $this->view('includes/profile1');
             ?>
         </div>
-        <div class="leave_container">
-            <div class="timeoff_head">
-                <div class="head_title">
-                    <p class="main_title">Time Off</p>
+
+        <div class="content">
+            <div>
+                <?php
+                $this->view('includes/header2')
+                ?>
+            </div>
+            <div class="leave_container">
+                <div class="timeoff_head">
+                    <div class="head_title">
+                        <p class="main_title">Time Off</p>
+                        <hr>
+                    </div>
+
+                    <div class="leave_card">
+
+                        <div class="card" title="Click for Request Lave">
+                            <a id="anchor" href="RequestleaveController">
+                                <p class="title">Casual Leaves</p>
+                                <div class="icon">
+                                    <i class="item" data-feather="calendar"></i>
+                                    <p class="remain" id="casual_remain">7</p>
+                                </div>
+                                <p>DAYS AVAILABLE</p>
+                            </a>
+
+                        </div>
+
+                        <div class="card" title="Click for Request Lave">
+                            <a id="anchor" href="RequestleaveController">
+                                <p class="title">Sick Leaves
+                                    <p>
+                                    <div class="icon">
+                                        <i class="item" data-feather="plus-square"></i>
+                                        <p class="remain" id="sick_remain">7</p>
+                                    </div>
+                                    <p>DAYS AVAILABLE</p>
+                            </a>
+                        </div>
+
+                        <div class="card" title="Click for Request Lave">
+                            <a id="anchor" href="RequestleaveController">
+                                <p class="title">Annual Leaves</p>
+                                <div class="icon">
+                                    <i class="item" data-feather="sun"></i>
+                                    <p class="remain" id="annual_remain">14</p>
+                                </div>
+                                <p>DAYS AVAILABLE</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="upcoming_timeoff">
+                    <div class="upcoming_header">
+                        <i class="item" data-feather="clipboard"></i>
+                        <p class="main_title">Upcoming Time Off</p>
+                    </div>
                     <hr>
-                </div>
 
-                <div class="leave_card">
-
-                    <div class="card" title="Click for Request Lave">
-                        <a id="anchor" href="RequestleaveController">
-                            <p class="title">Casual Leaves</p>
-                            <div class="icon">
-                                <i class="item" data-feather="calendar"></i>
-                                <p class="remain" id="casual_remain">7</p>
+                    <?php if(boolval($arr1)){ 
+                         for ($i = 0; $i < sizeof($arr1); $i++) { ?>
+                            <div class="upcoming_detail">
+                                <div class="icon">
+                                    <i class="item" data-feather="smile"></i>
+                                </div>
+    
+                                <div class="description">
+    
+                                    <div class="leave_name">
+                                        <p id="day"><?php print_r($arr1[$i]->date); ?></p>
+                                        <p id="reson">Request <?php print_r(ucfirst($arr1[$i]->leave_type)); ?> Leave</p>
+                                    </div>
+    
+                                    <div class="leave_status">
+                                        <p id="status"><i><?php print_r(ucfirst($arr1[$i]->leave_status)); ?></i></p>
+                                    </div>
+    
+    
+                                </div>
+    
                             </div>
-                            <p>DAYS AVAILABLE</p>
-                        </a>
+                        <?php
+                        } 
+                    } ?>
+                    
 
+                </div>
+                <div class="leave_history">
+                    <div class="history_header">
+                        <i class="item" data-feather="clock"></i>
+                        <p class="main_title">Leave History</p>
+                    </div>
+                    <hr>
+
+
+                    <div class="leave_type">
+                        <form action="">
+                            <select id="leave" name="leave">
+                                <option value="sick">Sick Leaves</option>
+                                <option value="casual">Casual Leaves</option>
+                                <option value="annual">Annual Leaves</option>
+                            </select>
+                            <a href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+                        </form>
                     </div>
 
-                    <div class="card" title="Click for Request Lave">
-                        <a id="anchor" href="RequestleaveController">
-                            <p class="title">Sick Leaves
-                            <p>
-                            <div class="icon">
-                                <i class="item" data-feather="plus-square"></i>
-                                <p class="remain" id="sick_remain">7</p>
-                            </div>
-                            <p>DAYS AVAILABLE</p>
-                        </a>
-                    </div>
+        
+                    <table id="leave_history_result">
 
-                    <div class="card" title="Click for Request Lave">
-                        <a id="anchor" href="RequestleaveController">
-                            <p class="title">Annual Leaves</p>
-                            <div class="icon">
-                                <i class="item" data-feather="sun"></i>
-                                <p class="remain" id="annual_remain">14</p>
-                            </div>
-                            <p>DAYS AVAILABLE</p>
-                        </a>
-                    </div>
+
+                        <?php 
+                        if(boolval($arr2)){
+
+                            // print_r($arr3);
+                            for ($i = 0; $i < sizeof($arr2); $i++) {?>
+                            
+                            <tr>
+                                <td> <?php print_r($arr2[$i]->date); ?></td>
+                                <td><?php print_r(ucfirst($arr2[$i]->leave_status)); ?></td>
+                                <td><?php print_r(ucfirst($arr2[$i]->leave_type))?> Leave</td>
+                                <?php $type = $arr2[$i]->leave_type ?>
+                                <td>+<?php print_r($arr3[$type]) ?></td>
+                            </tr>
+                            
+                    <?php
+                        }
+                        
+                        } ?>
+
+                    </table>
                 </div>
-            </div>
-
-            <div class="upcoming_timeoff">
-                <div class="upcoming_header">
-                    <i class="item" data-feather="clipboard"></i>
-                    <p class="main_title">Upcoming Time Off</p>
-                </div>
-                <hr>
-
-                <div class="upcoming_detail">
-                    <div class="icon">
-                        <i class="item" data-feather="smile"></i>
-                    </div>
-                    <div class="description">
-                        <!-- Dummy hard code value only -->
-                        <!-- this data filling after open this page and then get data fron database -->
-                        <div class="leave_name">
-                            <p id="day">May 25</p>
-                            <p id="reson">Request Casual Leave</p>
-                        </div>
-
-                        <div class="leave_status">
-                            <p id="status"><i>Pending</i></p>
-                        </div>
-
-
-                    </div>
-                </div>
-
-            </div>
-            <div class="leave_history">
-                <div class="history_header">
-                    <i class="item" data-feather="clock"></i>
-                    <p class="main_title">Leave History</p>
-                </div>
-                <hr>
-
-
-                <div class="leave_type">
-                    <form action="">
-                        <select id="leave" name="leave">
-                            <option value="sick">Sick Leaves</option>
-                            <option value="casual">Casual Leaves</option>
-                            <option value="annual">Annual Leaves</option>
-                        </select>
-                        <a href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
-                    </form>
-                </div>
-                <table id="leave_history_result">
-                    <tr>
-                        <th>Date</th>
-                        <th>Description</th>
-                        <th>Type</th>
-                        <th>Used</th>
-                    </tr>
-                    <tr>
-                        <td>23th August</td>
-                        <td>Doctor Meet</td>
-                        <td>Sick Leave</td>
-                        <td>+1</td>
-                    </tr>
-                    <tr>
-                        <td>23th August</td>
-                        <td>Doctor Meet</td>
-                        <td>Sick Leave</td>
-                        <td>+2</td>
-                    </tr>
-                </table>
             </div>
         </div>
     </div>
-</div>
-<script>
-    feather.replace()
-</script>
+    <script>
+        feather.replace()
+    </script>
 
-<!--</div>-->
-<?php //$this->view('includes/footer')?>
-<!--</div>-->
+    <!--</div>-->
+    <?php //$this->view('includes/footer')
+    ?>
+    <!--</div>-->
 </body>
 
 </html>
