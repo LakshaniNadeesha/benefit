8458c74dc178b9de525a1635eb1db885a7f0d6d2
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-03-23 16:01:02 +0530

    Update FomerEmployees.php

--- source/controller/FomerEmployees.php
+++ source/controller/FomerEmployees.php
@@ -3,20 +3,39 @@
 /**
  * former employees controller
  */
-class FomerEmployees extends Controller {
+class FomerEmployees extends Controller
+{
 
-function index(){
+    function index(){
         if(!Auth::logged_in())
         {
             $this->redirect('login');
         }
         $row =array();
-
         $user=new Employeedetails();
-        $row=$user->where('banned_employees',1);
-        //print_r($row);
+        $user_x=new PerformanceModel();
+        $row1=$user->where('banned_employees',1);
+      //  print_r($row1);
+       if(boolval($row1))
+       {
+         for($i=0;$i<sizeof($row1);$i++)
+        {
+            $rows = $user_x->where('employee_ID',$row1[$i]->employee_ID);
+            $row[$i]['employee_ID'] = $row1[$i]->employee_ID;
+            $row[$i]['employee_NIC'] = $row1[$i]->employee_NIC;
+            $row[$i]['first_name'] = $row1[$i]->first_name;
+            $row[$i]['last_name'] = $row1[$i]->last_name;
+            $row[$i]['gender'] = $row1[$i]->gender;
+            $row[$i]['profile_image'] = $row1[$i]->profile_image;
+            $row[$i]['details']=$rows[0];
+        
+       }
+   }
+
+       // print_r($row);
         $this->view('fomeremployees',['row'=>$row]);
 
     }
 
+ 
 }
