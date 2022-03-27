7c6bc85a0450d66ce0aca96a985ea487c12ad1c9
LakshaniNadeesha <2019cs105@stu.ucsc.lk>
2022-03-27 13:20:19 +0530

    Change Update Employee Controller

--- source/controller/UpdateemployeeController.php
+++ source/controller/UpdateemployeeController.php
@@ -87,43 +87,104 @@ class UpdateemployeeController extends Controller{
 		
 		return $validate;
 	}
+// function addfomeremp($id){
+
+//         $user=new UpdateemployeeModel();
+//         $data=$user->where('employee_ID',$id);
+//         if(count($_POST)>0){
+
+//             if(isset($_POST['submit'])){
+
+//                 $arr['street'] = filter_input(INPUT_POST, 'street', FILTER_SANITIZE_STRING);
+//                 $arr['city'] = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
+//                 $arr['province'] = filter_input(INPUT_POST, 'province', FILTER_SANITIZE_STRING);
+//                 $arr['marital_status'] = $_POST['marital'];
+//                 $arr['contact_number'] = $_POST['contact'];
+//                 $arr['supervisor_ID'] = $_POST['supervisor'];
+//                 $email =  filter_input(INPUT_POST, 'email_new', FILTER_SANITIZE_EMAIL);
+
+//                 $validate = $this->email_validate($email,$user);
+
+//                 if($validate){
+//                     $arr['email'] = filter_input(INPUT_POST, 'email_current', FILTER_SANITIZE_EMAIL);
+//                 }else{
+//                     $arr['email'] = filter_input(INPUT_POST, 'email_new', FILTER_SANITIZE_EMAIL);
+//                 }
+//                 $arr['banned_employees']=0;
+//                 $set = $user->update($id,$arr);
+
+//                 if((isset($set))){
+//                     $this->redirect('EmployeelistController');
+//                 }
+//             }
+//         }
+//         if(isset($_POST['cancel'])){
+//             // $this->view('employeelist',['rows'=>$data]);
+//             $this->redirect('EmployeelistController');
+//         }
+//         $this->view('updateemployeeformer',['rows'=>$data]);
+    
+//     }
 function addfomeremp($id){
 
-        $user=new UpdateemployeeModel();
-        $data=$user->where('employee_ID',$id);
-        if(count($_POST)>0){
 
-            if(isset($_POST['submit'])){
 
-                $arr['street'] = filter_input(INPUT_POST, 'street', FILTER_SANITIZE_STRING);
-                $arr['city'] = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
-                $arr['province'] = filter_input(INPUT_POST, 'province', FILTER_SANITIZE_STRING);
-                $arr['marital_status'] = $_POST['marital'];
-                $arr['contact_number'] = $_POST['contact'];
-                $arr['supervisor_ID'] = $_POST['supervisor'];
-                $email =  filter_input(INPUT_POST, 'email_new', FILTER_SANITIZE_EMAIL);
+    if(Auth::access('HR Manager')){
+    $user=new UpdateemployeeModel();
+    $data=$user->where('employee_ID',$id);
+    if(count($_POST)>0){
 
-                $validate = $this->email_validate($email,$user);
+        if(isset($_POST['submit'])){
 
-                if($validate){
-                    $arr['email'] = filter_input(INPUT_POST, 'email_current', FILTER_SANITIZE_EMAIL);
-                }else{
-                    $arr['email'] = filter_input(INPUT_POST, 'email_new', FILTER_SANITIZE_EMAIL);
-                }
-                $arr['banned_employees']=0;
-                $set = $user->update($id,$arr);
+            $arr['street'] = filter_input(INPUT_POST, 'street', FILTER_SANITIZE_STRING);
+            $arr['city'] = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
+            $arr['province'] = filter_input(INPUT_POST, 'province', FILTER_SANITIZE_STRING);
+            $arr['marital_status'] = $_POST['marital'];
+            $arr['contact_number'] = $_POST['contact'];
+            $arr['supervisor_ID'] = $_POST['supervisor'];
+            $email =  filter_input(INPUT_POST, 'email_new', FILTER_SANITIZE_EMAIL);
 
-                if((isset($set))){
-                    $this->redirect('EmployeelistController');
-                }
+            $validate = $this->email_validate($email,$user);
+
+            if($validate){
+                $arr['email'] = filter_input(INPUT_POST, 'email_current', FILTER_SANITIZE_EMAIL);
+            }else{
+                $arr['email'] = filter_input(INPUT_POST, 'email_new', FILTER_SANITIZE_EMAIL);
             }
+            $arr['banned_employees']=0;
+            $set = $user->update($id,$arr);
+            $data=$data[0];
+           // print_r($data->employee_NIC);
+            $stringa = 'currentdataformer/index/';
+            $stringa .= $data->employee_NIC;
+            $stringa .= '/';
+            $stringa .= $data->hired;
+            $stringa .= '/';
+            $stringa .= $data->first_name;
+            $stringa .= '/';
+            $stringa .= $data->last_name; 
+            //print_r($stringa);
+            $this->redirect($stringa);
+
+            // $this->redirect('currentdataformer/index/'print_r($data->NIC);'/'print_r($data->hired);'/'print_r($data->first_name);'/'print_r($data->last_name););
+            // if((isset($set))){
+            //     $data=$data[0];
+            //     $this->redirect('currentdataformer');
+            //     //print_r($data);
+            //    //$this->indexformer($data->employee_NIC,$data->hired,$data->first_name,$data->last_name); 
+            // }
         }
-        if(isset($_POST['cancel'])){
-            // $this->view('employeelist',['rows'=>$data]);
-            $this->redirect('EmployeelistController');
-        }
-        $this->view('updateemployeeformer',['rows'=>$data]);
+    }
+    if(isset($_POST['cancel'])){
+        // $this->view('employeelist',['rows'=>$data]);
+        $this->redirect('EmployeelistController');
+    }
+    $this->view('updateemployeeformer',['rows'=>$data]);
     
     }
+    else{
+        $this->redirect('404');
+    }
+}
     
 }
